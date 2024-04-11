beb03df8ebf10b6440c3e062ef22b990eb154670
Mnafyndiaye <111544993+Mnafyndiaye@users.noreply.github.com>
2024-04-11 22:53:24 +0000

    ssuita

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,26 @@
 
 // Importer Express
 const express = require('express');
+const bodyparser = require('body-parser');
+const mysql = require('mysql');
 
 // Créer une instance de l'application
 const app = express();
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'apinafy',
+  password: 'Passer123',
+  database: 'enseignements_db'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Erreur de connexion à la base de données MySQL :', err);
+    return;
+  }
+  console.log('Connecté à la base de données MySQL');
+});
 // Middleware pour le parsing du corps de la requête JSON
 app.use(express.json());
 
