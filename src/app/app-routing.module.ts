3c8ae8c9d9df3f9d133c3e9289b536159097a2af
Mnafyndiaye <111544993+Mnafyndiaye@users.noreply.github.com>
2024-04-11 19:55:41 +0000

    beine commt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -35,7 +35,7 @@ const routes: Routes = [
         component: CahierDeTexteComponent
       },
       {
-        path: 'profil-etudiant/evaluation-enseignement',
+        path: 'evaluation-enseignement',
         component: EvaluationEnseignementComponent
       },
       {
