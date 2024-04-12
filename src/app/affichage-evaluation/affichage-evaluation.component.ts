b242846c8e127fec8111e58dbc2c717d0db9fc97
seynqbou <zeynadiagne08@gmail.com>
2024-04-12 20:50:54 +0000

    Update affichage-evaluation.component.ts
    seynabafficheeval.ts

--- src/app/affichage-evaluation/affichage-evaluation.component.ts
+++ src/app/affichage-evaluation/affichage-evaluation.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Evaluation {
+    etudiant: string;
+    presentation: {
+        syllabus: string;
+        objectifs: string;
+    };
+    deroulement: {
+        conforme: string;
+        support: string;
+    };
+    conditions: {
+        salle: string;
+    };
+    suggestions: {
+        liens: string;
+        commentaires: string;
+    };
+}
 
 @Component({
   selector: 'app-affichage-evaluation',
   templateUrl: './affichage-evaluation.component.html',
   styleUrls: ['./affichage-evaluation.component.css']
 })
-export class AffichageEvaluationComponent {
+export class AffichageEvaluationComponent implements OnInit {
+    evaluations: Evaluation[];
 
+    ngOnInit() {
+        this.afficherEvaluations();
+    }
+
+    afficherEvaluations() {
+        this.evaluations = [
+            {
+                etudiant: "Anonymous",
+                presentation: {
+                    syllabus: "oui",
+                    objectifs: "oui"
+                },
+                deroulement: {
+                    conforme: "oui",
+                    support: "non"
+                },
+                conditions: {
+                    salle: "moyenne"
+                },
+                suggestions: {
+                    liens: "tres-lies",
+                    commentaires: "Très bon cours, mais pourrait bénéficier d'un meilleur support de cours."
+                }
+            }
+        ];
+    }
 }
+
