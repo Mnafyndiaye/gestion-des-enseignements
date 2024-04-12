2bc1759181c879d2c95dfb03b77e78493c0227e2
seynqbou <zeynadiagne08@gmail.com>
2024-04-12 21:45:42 +0000

    Update evaluation-enseignement.component.ts

--- src/app/evaluation-enseignement/evaluation-enseignement.component.ts
+++ src/app/evaluation-enseignement/evaluation-enseignement.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-evaluation-enseignement',
-  templateUrl: './evaluation-enseignement.component.html',
-  styleUrls: ['./evaluation-enseignement.component.css']
+  selector: 'app-formulaire-evaluation',
+  templateUrl: './formulaire-evaluation.component.html',
+  styleUrls: ['./formulaire-evaluation.component.css']
 })
-export class EvaluationEnseignementComponent {
+export class FormulaireEvaluationComponent implements OnInit {
+  evaluationForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.creerFormulaire();
+  }
+
+  creerFormulaire() {
+    this.evaluationForm = this.formBuilder.group({
+      syllabus: ['', Validators.required],
+      objectifs: ['', Validators.required],
+      conforme: ['', Validators.required],
+      support: ['', Validators.required],
+      salle: ['', Validators.required],
+      'liens-ec': ['', Validators.required],
+      commentaires: ['']
+    });
+  }
+
+  validerEvaluation() {
+    if (this.evaluationForm.valid) {
+      // Envoyer le formulaire ou effectuer d'autres actions nécessaires
+      console.log(this.evaluationForm.value);
+    } else {
+      // Afficher des messages d'erreur ou prendre d'autres mesures nécessaires
+      console.log('Le formulaire est invalide.');
+    }
+  }
 }
