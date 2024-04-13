9f09f6e479edf65452349aac9de6a0243cdedbfd
awacheikh20 <166763778+awacheikh20@users.noreply.github.com>
2024-04-12 19:46:27 -0700

    first commit

--- src/app/affichage-syllabus/affichage-syllabus.component.ts
+++ src/app/affichage-syllabus/affichage-syllabus.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Component, OnInit, HttpClient } from '@angular/core';
 
 @Component({
   selector: 'app-affichage-syllabus',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AffichageSyllabusComponent {
 
+  syllabusData: Array<{ cours: string, document: string}>;
+  httpClient: any;
+
+  constructor() {
+    this.syllabusData = [
+      { cours: "Java", document: "javasyllabus.pdf" },
+      { cours: "MSI", document: "msi_syllabus.docx" },
+      { cours: "HTML", document: "html_syllabus.pdf" }
+    ];
+  }
+
+  ngOnInit(): void {
+
+    this.httpClient.get('url-du-service')
+      .suscribe((data: { cours: string; document: string; }[]) => {
+        this.syllabusData = data;
+      },(error: any) => {
+        console.error('Erreur lors de la récupération des données du syllabus:', error);
+      });
+    
+  }
 }
